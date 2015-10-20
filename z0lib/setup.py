0b124219bd729a15058e4d8ac7ce33448e9f78f5
antoniov <info@shs-av.com>
2015-10-20 09:52:39 +0200

    [FIX] parseopt show help: fixed bug

--- z0lib/setup.py
+++ z0lib/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='z0lib',
-      version='0.1.9',
+      version='0.1.10',
       description='Bash zeroincombenze lib',
       classifiers=[
-          'Development Status :: 1 - Planning',
+          'Development Status :: 3 - Alpha',
       ])
