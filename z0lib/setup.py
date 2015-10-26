1befc3ccb6cd9ee973a43744d5adecc18cd49f2c
antoniov <info@shs-av.com>
2015-10-26 14:05:38 +0100

    [IMP] New z0lib features

--- z0lib/setup.py
+++ z0lib/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='z0lib',
-      version='0.1.14',
+      version='0.1.15',
       description='Bash zeroincombenze lib',
       classifiers=[
           'Development Status :: 1 - Planning',
