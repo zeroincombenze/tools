3fcb428c8bc964fad905e24dceda418288f970ad
antoniov <info@shs-av.com>
2015-11-11 09:48:47 +0100

    [RUN] nightly commit

--- z0lib/setup.py
+++ z0lib/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='z0lib',
-      version='0.1.17',
+      version='0.1.22',
       description='Bash zeroincombenze lib',
       classifiers=[
           'Development Status :: 1 - Planning',
