04f2d05bf81b03766f5d9f705ce0f8ddf26d139f
antoniov <info@shs-av.com>
2016-01-12 14:36:38 +0100

    [FIX] Library z0librc in product

--- product/setup.py
+++ product/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='product',
-      version='0.2.5',
+      version='0.2.10',
       description='Build a LAMP server',
       classifiers=[
           'Development Status :: 1 - Planning',
