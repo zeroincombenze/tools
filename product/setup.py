dcf0f05c201b2d419e6738ee3322cf5a0430ff30
zeroincombenze <info@shs-av.com>
2016-01-19 16:55:39 +0100

    [IMP] new vfcp and vfdiff command in travis emulator

--- product/setup.py
+++ product/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='product',
-      version='0.2.16',
+      version='0.2.17',
       description='Build a Odoo and LAMP server',
       classifiers=[
           'Development Status :: 1 - Planning',
