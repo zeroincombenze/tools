9f588e263ee1ad7c73571055921f4022c747539e
zeroincombenze <info@shs-av.com>
2016-02-22 17:30:59 +0100

    [IMP] Now travis emulator execute .travis.yml

--- travis_emulator/setup.py
+++ travis_emulator/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='travis_emulator',
-      version='0.1.33',
+      version='0.1.37',
       description='Travis CI emulator for local develop environment',
       classifiers=[
           'Development Status :: 1 - Planning',
