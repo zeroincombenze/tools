5692f562a6194c7a2f3311eb993b525f44e5f1d5
zeroincombenze <info@shs-av.com>
2016-02-24 18:52:03 +0100

    [IMP] new submodule for Odoo-Italy

--- product/setup.py
+++ product/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='product',
-      version='0.2.25',
+      version='0.2.28',
       description='Build a Odoo and LAMP server',
       classifiers=[
           'Development Status :: 1 - Planning',
