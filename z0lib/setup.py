6638ef0b42e994ae8011126e26b27aa0b9469022
zeroincombenze <info@shs-av.com>
2016-03-15 11:52:56 +0100

    [IMP] new repeated switch like -vv

--- z0lib/setup.py
+++ z0lib/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='z0lib',
-      version='0.1.34',
+      version='0.1.36',
       description='Bash zeroincombenze lib',
       classifiers=[
           'Development Status :: 1 - Planning',
