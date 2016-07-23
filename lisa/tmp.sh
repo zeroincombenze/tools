3d583055d2a3f05f30140252320de684c25462bc
zeroincombenze <info@shs-av.com>
2016-07-23 22:32:41 +0200

    [RUN] test nightly

--- lisa/tmp.sh
+++ lisa/tmp.sh
@@ -1,5 +1,14 @@
-q="-qb"
-p="a"
-if [ "$p" == "a" ] && [[  "$q" =~ q ]]; then
-  echo "ok"
-fi
+f() {
+  local p=$1
+  l=${p[0]}
+  o=${p[1]}
+  r=${p[2]}
+  echo "l=$l; o=$o; r=$r"
+}
+
+declare -a p
+p[0]="left"
+p[1]="op"
+p[2]="right"
+echo "l=${p[0]}; o==${p[1]}; r=${p[2]}"
+f "$p"
