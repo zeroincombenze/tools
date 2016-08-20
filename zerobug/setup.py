dfdc876e35aa2cfc70d75665130fa53a38c350b9
zeroincombenze <info@shs-av.com>
2016-08-20 16:19:27 +0200

    [IMP] zerobug new featues

--- zerobug/setup.py
+++ zerobug/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='zerobug',
       version='0.2.3',
-      description='Zeroincombenze® unit test library for python and bash programs',
+      description='Testing framework and tools for python and bash programs',
       classifiers=[
           'Development Status :: 1 - Planning',
       ])
