6372f7ff9a58d4fbd53e44e49b822ac4ad2a1e9a
zeroincombenze <info@shs-av.com>
2016-09-19 19:00:05 +0200

    [IMP] New lisa and z0libr features

--- z0lib/setup.py
+++ z0lib/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='z0lib',
-      version='0.1.38',
+      version='0.1.41',
       description='Bash zeroincombenze lib',
       classifiers=[
           'Development Status :: 1 - Planning',
