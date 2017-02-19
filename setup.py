fa7c7f799fdeba77d200bed579f55b2258d5a737
zeroincombenze <info@shs-av.com>
2017-02-19 22:14:30 +0100

    [IMP] New feature for travis emulator

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='tools',
+      version='0.1.0',
+      description='Sparse python source code',
+      classifiers=[
+          'Development Status :: 1 - Planning',
+      ])
