b849a5373d63589dd82740b69a626bd9adc75cce
zeroincombenze <info@shs-av.com>
2017-09-04 08:54:55 +0200

    [IMP] minor features

---
+++ clodoo/tests/clodoo_test_odoorc.sh
@@ -0,0 +1,203 @@
+#! /bin/bash
+# -*- coding: utf-8 -*-
+# Regression tests on clodoo
+#
+THIS=$(basename $0)
+TDIR=$(readlink -f $(dirname $0))
+for x in $TDIR $TDIR/.. $TDIR/../z0lib $TDIR/../../z0lib . .. /etc; do
+  if [ -e $x/z0librc ]; then
+    . $x/z0librc
+    Z0LIBDIR=$x
+    Z0LIBDIR=$(readlink -e $Z0LIBDIR)
+    break
+  fi
+done
+if [ -z "$Z0LIBDIR" ]; then
+  echo "Library file z0librc not found!"
+  exit 2
+fi
+TESTDIR=$(findpkg "" "$TDIR . .." "tests")
+RUNDIR=$(readlink -e $TESTDIR/..)
+Z0TLIBDIR=$(findpkg z0testrc "$TDIR $TDIR/.. $TDIR/../zerobug $TDIR/../../zerobug  . .. $HOME/dev")
+if [ -z "$Z0TLIBDIR" ]; then
+  echo "Library file z0testrc not found!"
+  exit 2
+fi
+. $Z0TLIBDIR
+Z0TLIBDIR=$(dirname $Z0TLIBDIR)
+__version__=0.0.1
+
+
+test_01() {
+    local s sts v
+    sts=0
+    opt_mult=0
+    declare -A TRES
+    TRES[6]=/etc/odoo/odoo-server.conf
+    TRES[7]=/etc/odoo/odoo-server.conf
+    TRES[8]=/etc/odoo/odoo-server.conf
+    TRES[9]=/etc/odoo/odoo-server.conf
+    TRES[10]=/etc/odoo/odoo.conf
+    for v in 6 7 8 9 10; do
+      if [ ${opt_dry_run:-0} -eq 0 ]; then
+        RES=$(build_odoo_param CONFN $v)
+      fi
+      test_result "config filename $v" "${TRES[$v]}" "$RES"
+      s=$?; [ ${s-0} -ne 0 ] && sts=$s
+    done
+    for v in 6 7 8 9 10; do
+      if [ ${opt_dry_run:-0} -eq 0 ]; then
+        if [ $v -eq 6 ]; then
+          RES=$(build_odoo_param CONFN $v.1)
+        else
+          RES=$(build_odoo_param CONFN $v.0)
+        fi
+      fi
+      test_result "config filename $v" "${TRES[$v]}" "$RES"
+      s=$?; [ ${s-0} -ne 0 ] && sts=$s
+    done
+    opt_mult=1
+    declare -A TRES
+    TRES[6]=/etc/odoo/odoo6-server.conf
+    TRES[7]=/etc/odoo/odoo7-server.conf
+    TRES[8]=/etc/odoo/odoo8-server.conf
+    TRES[9]=/etc/odoo/odoo9-server.conf
+    TRES[10]=/etc/odoo/odoo10.conf
+    for v in 6 7 8 9 10; do
+      if [ ${opt_dry_run:-0} -eq 0 ]; then
+        RES=$(build_odoo_param CONFN $v)
+      fi
+      test_result "config filename $v" "${TRES[$v]}" "$RES"
+      s=$?; [ ${s-0} -ne 0 ] && sts=$s
+    done
+    for v in 6 7 8 9 10; do
+      if [ ${opt_dry_run:-0} -eq 0 ]; then
+        if [ $v -eq 6 ]; then
+          RES=$(build_odoo_param CONFN $v.1)
+        else
+          RES=$(build_odoo_param CONFN $v.0)
+        fi
+      fi
+      test_result "config filename $v" "${TRES[$v]}" "$RES"
+      s=$?; [ ${s-0} -ne 0 ] && sts=$s
+    done
+    TRES[6]=/var/log/odoo/odoo6-server.log
+    TRES[7]=/var/log/odoo/odoo7-server.log
+    TRES[8]=/var/log/odoo/odoo8-server.log
+    TRES[9]=/var/log/odoo/odoo9-server.log
+    TRES[10]=/var/log/odoo/odoo10.log
+    for v in 6 7 8 9 10; do
+      if [ ${opt_dry_run:-0} -eq 0 ]; then
+        if [ $v -eq 6 ]; then
+          RES=$(build_odoo_param FLOG $v.1)
+        else
+          RES=$(build_odoo_param FLOG $v.0)
+        fi
+      fi
+      test_result "log filename $v" "${TRES[$v]}" "$RES"
+      s=$?; [ ${s-0} -ne 0 ] && sts=$s
+    done
+    TRES[6]=/var/run/odoo/odoo6-server.pid
+    TRES[7]=/var/run/odoo/odoo7-server.pid
+    TRES[8]=/var/run/odoo/odoo8-server.pid
+    TRES[9]=/var/run/odoo/odoo9-server.pid
+    TRES[10]=/var/run/odoo/odoo10.pid
+    for v in 6 7 8 9 10; do
+      if [ ${opt_dry_run:-0} -eq 0 ]; then
+        if [ $v -eq 6 ]; then
+          RES=$(build_odoo_param FPID $v.1)
+        else
+          RES=$(build_odoo_param FPID $v.0)
+        fi
+      fi
+      test_result "pid filename $v" "${TRES[$v]}" "$RES"
+      s=$?; [ ${s-0} -ne 0 ] && sts=$s
+    done
+    TRES[6]=/etc/init.d/odoo6-server
+    TRES[7]=/etc/init.d/odoo7-server
+    TRES[8]=/etc/init.d/odoo8-server
+    TRES[9]=/etc/init.d/odoo9-server
+    TRES[10]=/etc/init.d/odoo10
+    for v in 6 7 8 9 10; do
+      if [ ${opt_dry_run:-0} -eq 0 ]; then
+        if [ $v -eq 6 ]; then
+          RES=$(build_odoo_param SVCNAME $v.1)
+        else
+          RES=$(build_odoo_param SVCNAME $v.0)
+        fi
+      fi
+      test_result "script name $v" "${TRES[$v]}" "$RES"
+      s=$?; [ ${s-0} -ne 0 ] && sts=$s
+    done
+    return $sts
+}
+
+Z0BUG_setup() {
+    local VERSION=9.0
+    local ODOO_REPO=local/odoo
+    LCLTEST_PRJNAME="Odoo"
+    LCLTEST_REPOSNAME=l10n_italy
+    LCLTEST_PKGNAME=l10n_it_base
+    LCLTEST_TMPDIR0=~/dev/odoo/$VERSION
+    LCLTEST_PRJPATH=$LCLTEST_TMPDIR0/$LCLTEST_REPOSNAME
+    LCLTEST_TMPDIR=$LCLTEST_PRJPATH/$LCLTEST_PKGNAME
+    LCLTEST_TMPDIR2=$LCLTEST_PRJPATH/__unported__/$LCLTEST_PKGNAME
+    mkdir -p $LCLTEST_TMPDIR0
+    mkdir -p $LCLTEST_PRJPATH
+    mkdir -p $LCLTEST_TMPDIR
+    mkdir -p $LCLTEST_TMPDIR2
+    touch $LCLTEST_TMPDIR/__openerp__.py
+    touch $LCLTEST_TMPDIR2/__openerp__.py
+    LCLTEST_SETUP=__openerp__.py
+    LCLTEST_MQT_PATH=$HOME/maintainer-quality-tools/travis
+    if [ ! -f /etc/odoo/odoo9-server.conf ]; then
+      LCLTEST_ODOO9_SERVER=/etc/odoo/odoo9-server.conf
+      touch $LCLTEST_ODOO9_SERVER
+    fi
+    if [ -d ~/$VERSION/dependencies ]; then rm -fR ~/$VERSION/dependencies; fi
+    if [ -L ~/$VERSION/dependencies ]; then rm -f ~/$VERSION/dependencies; fi
+}
+
+Z0BUG_teardown() {
+    rm -fR $LCLTEST_TMPDIR2
+    rm -fR $LCLTEST_TMPDIR
+    rm -fR $LCLTEST_PRJPATH
+    rm -fR $LCLTEST_TMPDIR0
+    if [ -n "$LCLTEST_ODOO9_SERVER" ]; then
+      rm -f $LCLTEST_ODOO9_SERVER
+    fi
+    if [ -f $FOUT ]; then rm -f $FOUT; fi
+    if [ -f $FTEST ]; then rm -f $FTEST; fi
+}
+
+
+Z0BUG_init
+parseoptest -l$TESTDIR/test_clodoo.log "$@ -J"
+sts=$?
+if [ $sts -ne 127 ]; then
+  exit $sts
+fi
+opt_oelib=1
+if [ ${opt_oelib:-0} -ne 0 ]; then
+  ODOOLIBDIR=$(findpkg travisrc "$TDIR $TDIR/.. $TDIR/../clodoo $TDIR/../../clodoo . .. $HOME/dev")
+  if [ -z "$ODOOLIBDIR" ]; then
+    echo "Library file odoorc not found!"
+    exit 2
+  fi
+  . $ODOOLIBDIR
+fi
+if [ ${opt_tlib:-0} -ne 0 ]; then
+  TRAVISLIBDIR=$(findpkg travisrc "$TDIR $TDIR/.. $TDIR/../travis_emulator $TDIR/../../travis_emulator . .. $HOME/dev")
+  if [ -z "$TRAVISLIBDIR" ]; then
+    echo "Library file travisrc not found!"
+    exit 2
+  fi
+  . $TRAVISLIBDIR
+fi
+UT1_LIST=
+UT_LIST=""
+if [ "$(type -t Z0BUG_setup)" == "function" ]; then Z0BUG_setup; fi
+Z0BUG_main_file "$UT1_LIST" "$UT_LIST"
+sts=$?
+if [ "$(type -t Z0BUG_teardown)" == "function" ]; then Z0BUG_teardown; fi
+exit $sts
---
+++ travis_emulator/gen_addons_table.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+"""
+This script replaces markers in the README.md files
+of an OCA repository with the list of addons present
+in the repository. It preserves the marker so it
+can be run again.
+The script must be run from the root of the repository,
+where the README.md file can be found.
+Markers in README.md must have the form:
+[//]: # (addons)
+does not matter, will be replaced by the script
+[//]: # (end addons)
+"""
+
+from __future__ import print_function
+import ast
+import os
+import re
+import sys
+
+
+MARKERS = r'(\[//\]: # \(addons\))|(\[//\]: # \(end addons\))'
+MANIFESTS = ('__openerp__.py', '__manifest__.py')
+__version__ = "0.1.0.3"
+
+
+class UserError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+def sanitize_cell(s):
+    if not s:
+        return ''
+    s = ' '.join(s.split())
+    return s
+
+
+def render_markdown_table(header, rows):
+    table = []
+    rows = [header, ['---'] * len(header)] + rows
+    for row in rows:
+        table.append(' | '.join(row))
+    return '\n'.join(table)
+
+
+def print_addons(header, rows_available, rows_unported):
+    addons = []
+    if rows_available:
+        addons.extend([
+            '\n',
+            '\n',
+            'Available addons\n',
+            '----------------\n',
+            render_markdown_table(header, rows_available),
+            '\n'
+        ])
+    if rows_unported:
+        addons.extend([
+            '\n',
+            '\n',
+            'Unported addons\n',
+            '---------------\n',
+            render_markdown_table(header, rows_unported),
+            '\n'
+        ])
+    addons.append('\n')
+    for line in addons:
+        sys.stdout.write(line)
+
+
+def replace_in_readme(readme_path, header, rows_available, rows_unported):
+    readme = open(readme_path).read()
+    parts = re.split(MARKERS, readme, flags=re.MULTILINE)
+    if len(parts) != 7:
+        raise UserError('Addons markers not found or incorrect in %s' %
+                        readme_path)
+    addons = []
+    if rows_available:
+        addons.extend([
+            '\n',
+            '\n',
+            'Available addons\n',
+            '----------------\n',
+            render_markdown_table(header, rows_available),
+            '\n'
+        ])
+    if rows_unported:
+        addons.extend([
+            '\n',
+            '\n',
+            'Unported addons\n',
+            '---------------\n',
+            render_markdown_table(header, rows_unported),
+            '\n'
+        ])
+    addons.append('\n')
+    parts[2:5] = addons
+    parts = [p.encode('utf-8') if isinstance(p, unicode) else p for p in parts]
+    readme = ''.join(parts)
+    open(readme_path, 'w').write(readme)
+
+def get_values_from_manifest(addon_path, manifest_path):
+    manifest = ast.literal_eval(open(manifest_path).read())
+    addon_name = os.path.basename(addon_path)
+    link = '[%s](%s/)' % (addon_name, addon_path)
+    version = manifest.get('version') or ''
+    summary = manifest.get('summary') or manifest.get('name')
+    summary = sanitize_cell(summary)
+    installable = manifest.get('installable', True)
+    if not installable:
+        version = version + ' (unported)'
+    return [link, version, summary],  installable
+
+def gen_addons_table(args):
+    inline = True
+    original_OCA = False
+    if len(args) == 0 or args[0] != "inline":
+        inline = False
+        readme_path = 'README.md'
+        if not os.path.isfile(readme_path):
+            raise UserError('%s not found' % readme_path)
+    elif len(args) >= 2 and os.path.isdir(args[1]):
+         original_OCA = args[1]
+    # list addons in . and __unported__
+    addon_paths = []  # list of (addon_path, unported)
+    for addon_path in os.listdir('.'):
+        addon_paths.append((addon_path, False))
+    unported_directory = '__unported__'
+    if os.path.isdir(unported_directory):
+        for addon_path in os.listdir(unported_directory):
+            addon_path = os.path.join(unported_directory, addon_path)
+            addon_paths.append((addon_path, True))
+    addon_paths = sorted(addon_paths, lambda x, y: cmp(x[0], y[0]))
+    # load manifestsif original_OCA:
+    if original_OCA:
+        header = ('addon', 'version', 'OCA version', 'summary')
+    else:
+        header = ('addon', 'version', 'summary')
+    rows_available = []
+    rows_unported = []
+    rows_original = []
+    for addon_path, unported in addon_paths:
+        for manifest_file in MANIFESTS:
+            manifest_path = os.path.join(addon_path, manifest_file)
+            has_manifest = os.path.isfile(manifest_path)
+            if has_manifest:
+                break
+        if has_manifest:
+            row, installable = get_values_from_manifest(addon_path,
+                                                        manifest_path)
+            if unported and installable:
+                raise UserError('%s is in __unported__ but is marked '
+                                'installable.' % addon_path)
+            o_row = ['', ' N/D ', '']
+            if original_OCA:
+                addon_name = os.path.basename(addon_path)
+                OCA_addon_path = os.path.join(original_OCA, addon_name)
+                for o_manifest_file in MANIFESTS:
+                    o_manifest_path = os.path.join(OCA_addon_path,
+                                                   o_manifest_file)
+                    o_has_manifest = os.path.isfile(o_manifest_path)
+                    if o_has_manifest:
+                        break
+                if o_has_manifest:
+                    o_row, o_installable = get_values_from_manifest(
+                        OCA_addon_path, o_manifest_path)
+                if row[1] != o_row[1]:
+                    row.insert(2, o_row[1])
+                else:
+                    row.insert(2, 'Same')
+            if installable:
+                rows_available.append(row)
+            else:
+                rows_unported.append(row)
+    if not inline:
+        # replace table in README.md
+        replace_in_readme(readme_path, header, rows_available, rows_unported)
+    else:
+        print_addons(header, rows_available, rows_unported)
+
+
+def main(args):
+    try:
+        gen_addons_table(args)
+    except UserError as e:
+        print(e.msg)
+        exit(1)
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    main(args)
