8da4705393e8c1467531550aed04aa5fda1c0a5e
zeroincombenze <info@shs-av.com>
2017-10-28 06:41:39 +0200

    [IMP] Travis emulator new features

---
+++ travis_emulator/tests/res/test_autopep_07.py
@@ -0,0 +1,9 @@
+# Copyright antoniov <antoniov@libero.it>
+#     (C) antoniov <antoniov@libero.it>
+# Copyright (C) antoniov <antoniomaria.vigliotti@gmail.com>
+# Copyright 2013-2017 antoniov <antoniomaria.vigliotti@gmail.com>
+#
+# [2013: antoniov] First version
+# 
+
+__version__='0.1.2'
