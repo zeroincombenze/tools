f32dad1a6c1a8e801a85d6642c3b5b36e151d98e
zeroincombenze <info@shs-av.com>
2018-04-03 07:20:17 +0200

    [IMP] New enhancements in order to manage Odoo 11

--- maintainer-quality-tools/git/run_pylint.py
+++ maintainer-quality-tools/git/run_pylint.py
@@ -20,7 +20,7 @@ try:
 except ImportError:
     import configparser as ConfigParser
 
-__version__ = '0.2.1.17'
+__version__ = '0.2.1.18'
 CLICK_DIR = click.Path(exists=True, dir_okay=True, resolve_path=True)
 
 
