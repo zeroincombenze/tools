45f601ad3090113578ec73159c8381df520401b0
zeroincombenze <info@shs-av.com>
2018-05-04 20:40:16 +0200

    [IMP] New enhancements in order to manage Odoo 11

--- maintainer-quality-tools/travis/run_pylint.py
+++ maintainer-quality-tools/travis/run_pylint.py
@@ -20,7 +20,7 @@ try:
 except ImportError:
     import configparser as ConfigParser
 
-__version__ = '0.2.1.40'
+__version__ = '0.2.1.41'
 CLICK_DIR = click.Path(exists=True, dir_okay=True, resolve_path=True)
 
 
