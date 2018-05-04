6b93552ba0cdc5acf4693bd4366f9e99c3cbe7b3
zeroincombenze <info@shs-av.com>
2018-05-04 21:56:11 +0200

    [IMP] New enhancements in order to manage Odoo 11

--- travis_emulator/setup.py
+++ travis_emulator/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='travis_emulator',
-      version='0.1.86.2',
+      version='0.2.1.21',
       description='Travis CI emulator for local develop environment',
       classifiers=[
           'Development Status :: 1 - Planning',
