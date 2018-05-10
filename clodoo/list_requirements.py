b1d60ae3530c467dc600e824acc84715df9d8f8d
zeroincombenze <info@shs-av.com>
2018-05-10 09:38:56 +0200

    [IMP] New enhancements in order to manage Odoo 11

--- clodoo/list_requirements.py
+++ clodoo/list_requirements.py
@@ -7,7 +7,7 @@ import re
 import z0lib
 
 
-__version__ = '0.3.6.39'
+__version__ = '0.3.6.40'
 
 #
 # known incompantibilities:
--- clodoo/tests/clodoo_test_02.py
+++ clodoo/tests/clodoo_test_02.py
@@ -24,7 +24,7 @@ from clodoo import clodoo
 from clodoo.clodoolib import crypt
 
 
-__version__ = "0.3.6.39"
+__version__ = "0.3.6.40"
 
 
 MODULE_ID = 'clodoo'
