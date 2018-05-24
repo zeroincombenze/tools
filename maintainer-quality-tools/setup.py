21750b294fb5e753cd2d79d673090939c19f2951
zeroincombenze <info@shs-av.com>
2018-05-24 22:05:27 +0200

    [RUN] test nightly

--- maintainer-quality-tools/setup.py
+++ maintainer-quality-tools/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='maintainer-quality-tools',
-      version='0.2.1.46',
+      version='0.2.1.47',
       description='QA Tools for Odoo maintainers (MQT)',
       classifiers=[
           'Development Status :: 1 - Planning',
