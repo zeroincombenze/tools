c8d5862536fb64bd85c200b77cc0cb8ea451eb70
zeroincombenze <info@shs-av.com>
2018-06-03 20:35:42 +0200

    [IMP] Best recognition of Odoo requirements

--- maintainer-quality-tools/git/run_pylint.py
+++ maintainer-quality-tools/git/run_pylint.py
@@ -20,7 +20,7 @@ try:
 except ImportError:
     import configparser as ConfigParser
 
-__version__ = '0.2.1.52'
+__version__ = '0.2.1.53'
 CLICK_DIR = click.Path(exists=True, dir_okay=True, resolve_path=True)
 
 
