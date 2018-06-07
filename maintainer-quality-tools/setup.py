0df1713574d8bc415560021c312ed01daae71b87
zeroincombenze <info@shs-av.com>
2018-06-07 11:43:08 +0200

    [IMP] Best recognition of Odoo requirements

--- maintainer-quality-tools/setup.py
+++ maintainer-quality-tools/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='maintainer-quality-tools',
-      version='0.2.1.53',
+      version='0.2.1.54',
       description='QA Tools for Odoo maintainers (MQT)',
       classifiers=[
           'Development Status :: 1 - Planning',
