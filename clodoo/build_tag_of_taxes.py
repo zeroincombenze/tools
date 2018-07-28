280ceb9cc7c6a1612682aad3890bf98082b5a81a
zeroincombenze <info@shs-av.com>
2018-07-28 18:53:03 +0200

    [IMP] MQT Minor features

--- clodoo/build_tag_of_taxes.py
+++ clodoo/build_tag_of_taxes.py
@@ -7,7 +7,7 @@ from z0lib import parseoptargs
 # import clodoo
 
 
-__version__ = '0.3.7.15'
+__version__ = '0.3.7.16'
 
 
 def read_csv_file(csv_fn):
--- clodoo/upd_oemod.py
+++ clodoo/upd_oemod.py
@@ -9,7 +9,7 @@ import oerplib
 from z0lib import parseoptargs
 
 
-__version__ = "0.3.7.15"
+__version__ = "0.3.7.16"
 
 
 def initialize_params(ctx):
