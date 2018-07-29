67999c98db206ebb147159ea6a7684089d5c92ab
zeroincombenze <info@shs-av.com>
2018-07-29 10:33:58 +0200

    [IMP] MQT Minor features

--- maintainer-quality-tools/travis/run_pylint.py
+++ maintainer-quality-tools/travis/run_pylint.py
@@ -20,7 +20,7 @@ try:
 except ImportError:
     import configparser as ConfigParser
 
-__version__ = '0.2.1.62'
+__version__ = '0.2.1.63'
 CLICK_DIR = click.Path(exists=True, dir_okay=True, resolve_path=True)
 
 
