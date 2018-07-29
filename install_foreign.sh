e434fa435ea478374ed22bd1ace2528bb70c21ac
zeroincombenze <info@shs-av.com>
2018-07-29 15:01:20 +0200

    [IMP] MQT Minor features

--- install_foreign.sh
+++ install_foreign.sh
@@ -1,8 +1,8 @@
-# __version__=0.1.4
-RFLIST__travis_emulator="beauty dist_pkg gen_addons_table.py please please.man please.py prjdiff replica.sh topep8 topep8.py to_pep8.2p8 to_pep8.py travis travisrc vfcp vfdiff wok_doc wok_doc.py"
+# __version__=0.1.5
+RFLIST__travis_emulator="dist_pkg gen_addons_table.py please please.man please.py prjdiff replica.sh topep8 topep8.py to_pep8.2p8 to_pep8.py travis travisrc vfcp vfdiff wok_doc wok_doc.py"
 RFLIST__clodoo="addsubm.sh awsfw clodoo.py clodoocore.py clodoolib.py inv2draft_n_restore.py list_requirements.py manage_db manage_odoo odoorc oe_watchdog run_odoo_debug.sh odoo_skin.sh set_odoover_confn transodoo.py transodoo.csv upd_oemod.py"
 RFLIST__zar="pg_db_active"
-RFLIST__z0lib="Makefile z0lib.py z0librc"
+RFLIST__z0lib="z0lib.py z0librc"
 RFLIST__zerobug="z0testrc"
 RFLIST__wok_code="cvt_script"
 RFLIST__lisa="lisa lisa.conf.sample lisa.man odoo.lish python.lish"
