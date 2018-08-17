4016ff22cb03df3283180775fa0894c6c039e1ad
zeroincombenze <info@shs-av.com>
2018-08-17 19:12:00 +0200

    [IMP] Minor features of clodoo

--- maintainer-quality-tools/setup.py
+++ maintainer-quality-tools/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='maintainer-quality-tools',
-      version='0.2.1.67',
+      version='0.2.1.68',
       description='QA Tools for Odoo maintainers (MQT)',
       classifiers=[
           'Development Status :: 1 - Planning',
