628ec0394e41b180e1294feb73f2ec4b437a14e9
zeroincombenze <info@shs-av.com>
2018-11-25 20:54:03 +0100

    [FIX] Tools autotest

--- install_tools.sh
+++ install_tools.sh
@@ -1,4 +1,4 @@
-# __version__=0.1.16
+# __version__=0.1.18
 THIS=$(basename "$0")
 TDIR=$(readlink -f $(dirname $0))
 if [[ $1 =~ -.*h ]]; then
@@ -8,13 +8,13 @@ if [[ $1 =~ -.*h ]]; then
   echo "  -q  quiet mode"
   exit 0
 fi
-RFLIST__travis_emulator="cvt_csv_2_rst cvt_csv_2_html dist_pkg gen_addons_table.py gen_readme.py odoo_translation.py please please.man please.py prjdiff replica.sh topep8 topep8.py to_pep8.2p8 to_pep8.py travis travisrc vfcp vfdiff wok_doc wok_doc.py"
+RFLIST__travis_emulator="cvt_csv_2_rst.py cvt_csv_2_xml.py dist_pkg gen_addons_table.py gen_readme.py odoo_translation.py please please.man please.py prjdiff replica.sh topep8 topep8.py to_pep8.2p8 to_pep8.py travis travisrc vfcp vfdiff wok_doc wok_doc.py"
 RFLIST__clodoo="awsfw clodoo.py clodoocore.py clodoolib.py inv2draft_n_restore.py list_requirements.py manage_db manage_odoo manage_odoo.man odoo_install_repository odoorc oe_watchdog run_odoo_debug odoo_skin.sh set_odoover_confn transodoo.py transodoo.csv upd_oemod.py"
 RFLIST__zar="pg_db_active"
-RFLIST__z0lib="z0lib.py z0librc"
+RFLIST__z0lib="z0librun.py z0librc"
 RFLIST__zerobug="z0testrc"
 RFLIST__wok_code="cvt_script"
-RFLIST__lisa="lisa lisa.conf.sample lisa.man lisa_bld_ods kbase odoo-server_Debian odoo-server_RHEL"
+RFLIST__lisa="lisa lisa.conf.sample lisa.man lisa_bld_ods kbase/*.lish odoo-server_Debian odoo-server_RHEL"
 RFLIST__tools="odoo_default_tnl.csv templates"
 SRCPATH=
 DSTPATH=
@@ -26,13 +26,18 @@ if [ -z "$SRCPATH" -o -z "$DSTPATH" ]; then
   echo "Invalid environment"
   exit 1
 fi
-for pkg in travis_emulator clodoo zar z0lib zerobug wok_code lisa tools; do
+# for pkg in travis_emulator clodoo zar z0lib zerobug wok_code lisa tools; do
+for pkg in lisa; do
   l="RFLIST__$pkg"
   flist=${!l}
   [[ ! $1 =~ -.*q ]] && echo "[$pkg=$flist]"
   for fn in $flist; do
     [ "$pkg" != "tools" ] && src="$SRCPATH/${pkg}/$fn" || src="$SRCPATH/$fn"
     tgt="$DSTPATH/$fn"
+    if $(echo "$src"|grep -Eq "\*"); then
+      src=$(dirname "$src")
+      tgt=$(dirname "$tgt")
+    fi
     if [ ! -f "$src" -a ! -d "$src" ]; then
       echo "File $src not found!"
     elif [[ ! -L "$tgt" || $1 =~ -.*p ]]; then
@@ -45,7 +50,7 @@ for pkg in travis_emulator clodoo zar z0lib zerobug wok_code lisa tools; do
     fi
   done
 done
-for fn in addsubm.sh run_odoo_debug.sh; do
+for fn in odoo_install_repository run_odoo_debug; do
   tgt="$DSTPATH/$fn"
   if [ -L "$tgt"  -o -f "$tgt" ]; then
     [[ ! $1 =~ -.*q ]] && echo "\$ rm -f $tgt"
---
+++ z0lib/z0librun.py
@@ -0,0 +1,274 @@
+# -*- coding: utf-8 -*-
+#
+#
+#    Copyright (C) SHS-AV s.r.l. (http://www.shs-av.com/)
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+"""@mainpage
+Zeroincombenze® general purpose library
+=======================================
+
+Library with various functionalities for python and bash programs.
+Area managed:
+- parseoptargs: line command parser; expands python argparse and adds same
+                functionalities to bash scripts
+- xuname:       platform recognition (only bash); return info about host
+- tracelog:     manage tracelog (only bash)
+- run_traced:   execute (or dry_run) shell command (only bash)
+- findpkg:      find package in file system (only bash)
+- CFG:          manage a dictionay value from config file
+               like python ConfigParser (only bash)
+
+@author: Antonio M. Vigliotti antoniomaria.vigliotti@gmail.com
+"""
+
+# import pdb
+import os
+import argparse
+import inspect
+try:
+    import ConfigParser
+except ImportError:
+    import configparser as ConfigParser
+from os0 import os0
+
+
+# Apply for configuration file (True/False)
+APPLY_CONF = True
+# Default configuration file (i.e. myfile.conf or False for default)
+CONF_FN = "./clodoo.conf"
+# Read Odoo configuration file (False or /etc/odoo-server.conf)
+ODOO_CONF = ["/etc/odoo/odoo-server.conf",
+             "/etc/odoo-server.conf",
+             "/etc/openerp/openerp-server.conf",
+             "/etc/openerp-server.conf",
+             "/etc/odoo/openerp-server.conf",
+             "/etc/openerp/odoo-server.conf"]
+# Read Odoo configuration file (False or /etc/openerp-server.conf)
+OE_CONF = False
+DEFDCT = {}
+__version__ = "0.2.8"
+
+
+class CountAction(argparse.Action):
+
+    def __init__(self,
+                 option_strings,
+                 dest,
+                 default=None,
+                 required=False,
+                 help=None):
+        super(CountAction, self).__init__(
+            option_strings=option_strings,
+            dest=dest,
+            nargs=0,
+            default=default,
+            required=required,
+            help=help)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        new_count = argparse._ensure_value(namespace, self.dest, 0)
+        if option_string != '-q':
+            new_count += 1
+        setattr(namespace, self.dest, new_count)
+
+
+class parseoptargs():
+
+    def __init__(self, *args, **kwargs):
+        self.parser = argparse.ArgumentParser(description=args[0],
+                                              epilog=args[1])
+        if 'version' in kwargs:
+            self.version = kwargs['version']
+        self.param_list = []
+
+    def add_argument(self, *args, **kwargs):
+        """Add argument to param list
+        @param *args    positional arg; format may be '-x', '--xx' or 'xxx'
+        @param action   action done when option is encountered; may be
+                    - 'store' ('=' in bash); get value follows param
+                        i.e. --opt=value -l value
+                    - 'store_const'  (then constant value in bash);
+                        set constant value from 'const' param
+                    - 'store_true' 'store_false': same above, boolean value
+                    - 'append' (not implemented in bash)
+                    - 'count' ('+' in bash); how many times option occurs
+                    - CountAction; same ad 'count' but may reset by '-q' option
+                    - 'help'
+                    - 'version'
+        @param dest     internal name of param
+        @param default  declare default value
+        @param metavar  complete help for action_store
+        """
+        if len(args) and args[0][0] != '-':
+            self.param_list.append(args[0])
+            self.parser.add_argument(*args, **kwargs)
+        elif args[0] != '-h' and args[0] != '--help':
+            if len(args) == 1:
+                if args[0] == '-n' or args[0] == '--dry-run':
+                    self.parser.add_argument(
+                        '-n', '--dry-run',
+                        help='do nothing (dry-run)',
+                        action='store_true',
+                        dest='dry_run',
+                        default=False)
+                    self.param_list.append('dry_run')
+                elif args[0] == '-q' or args[0] == '--quite':
+                    self.parser.add_argument(
+                        '-q', '--quiet',
+                        help="silent mode",
+                        action=CountAction,
+                        dest="opt_verbose",)
+                    self.param_list.append('opt_verbose')
+                elif args[0] == '-V' or args[0] == '--version':
+                    self.parser.add_argument(
+                        '-V', '--version',
+                        action='version',
+                        version=self.version)
+                elif args[0] == '-v' or args[0] == '--verbose':
+                    self.parser.add_argument(
+                        '-v', '--verbose',
+                        help="verbose mode",
+                        action=CountAction,
+                        dest="opt_verbose")
+                    self.param_list.append('opt_verbose')
+                else:
+                    raise NotImplementedError
+            else:
+                if 'dest' in kwargs:
+                    self.param_list.append(kwargs['dest'])
+                self.parser.add_argument(*args, **kwargs)
+
+    def default_conf(self, ctx):
+        return DEFDCT
+
+    def get_this_fqn(self):
+        i = 1
+        valid = False
+        while not valid:
+            this_fqn = os.path.abspath(inspect.stack()[i][1])
+            this = os0.nakedname(os.path.basename(this_fqn))
+            if this in ("__init__", "pdb", "cmd", "z0testlib", "z0lib"):
+                i += 1
+            else:
+                valid = True
+        return this_fqn
+
+    # def create_params_dict(self, ctx):
+    #     """Create all params dictionary"""
+    #     ctx = self.create_def_params_dict(ctx)
+    #     return ctx
+    def create_params_dict(self, ctx):
+        """Create default params dictionary"""
+        opt_obj = ctx.get('_opt_obj', None)
+        # conf_obj = ctx.get('_conf_obj', None)
+        # s = "options"
+        # if conf_obj:                                       # pragma: no cover
+        #     if not conf_obj.has_section(s):
+        #         conf_obj.add_section(s)
+        #     for p in LX_CFG_S:
+        #         ctx[p] = conf_obj.get(s, p)
+        #     for p in LX_CFG_B:
+        #         ctx[p] = conf_obj.getboolean(s, p)
+        # else:
+        #     DEFDCT = self.default_conf(ctx)
+        #     for p in LX_CFG_S:
+        #         if p in DEFDCT:
+        #             ctx[p] = DEFDCT[p]
+        #     for p in LX_CFG_B:
+        #         if p in DEFDCT:
+        #             ctx[p] = DEFDCT[p]
+        if opt_obj:
+            for p in self.param_list:
+                ctx[p] = getattr(opt_obj, p)
+        return ctx
+
+    def read_config(self, ctx):
+        """Read both user configuration and local configuration."""
+        if not ctx.get('conf_fn', None):
+            if CONF_FN:
+                ctx['conf_fn'] = CONF_FN
+            else:
+                ctx['conf_fn'] = "./" + ctx['caller'] + ".conf"
+        conf_obj = ConfigParser.SafeConfigParser(self.default_conf(ctx))
+        if ODOO_CONF:
+            if isinstance(ODOO_CONF, list):
+                found = False
+                for f in ODOO_CONF:
+                    if os.path.isfile(f):
+                        ctx['conf_fns'] = (f, ctx['conf_fn'])
+                        found = True
+                        break
+                if not found:
+                    ctx['conf_fns'] = ctx['conf_fn']
+            else:
+                if os.path.isfile(ODOO_CONF):
+                    ctx['conf_fns'] = (ODOO_CONF, ctx['conf_fn'])
+                elif os.path.isfile(OE_CONF):
+                    ctx['conf_fns'] = (OE_CONF, ctx['conf_fn'])
+                else:
+                    ctx['conf_fns'] = ctx['conf_fn']
+        else:
+            ctx['conf_fns'] = ctx['conf_fn']
+        ctx['conf_fns'] = conf_obj.read(ctx['conf_fns'])
+        ctx['_conf_obj'] = conf_obj
+        return ctx
+
+    def parseoptargs(self, arguments,
+                     apply_conf=APPLY_CONF, version=None, tlog=None, doc=None):
+        """Parse command-line options.
+        @param arguments list of arguments; should argv from command line
+        @param version   software version to displya with -V option
+                         in bash version reports __version__ variable of script
+        """
+        ctx = {}
+        this_fqn = self.get_this_fqn()
+        ctx['this_fqn'] = this_fqn
+        this = os0.nakedname(os.path.basename(this_fqn))
+        ctx['this'] = this
+        if os.isatty(0):
+            ctx['run_daemon'] = False
+        else:                                               # pragma: no cover
+            ctx['run_daemon'] = True
+        ctx['run_tty'] = os.isatty(0)
+        if tlog:
+            ctx['tlog'] = tlog
+        else:
+            ctx['tlog'] = this + '.log'
+        # running autotest
+        if version is None:
+            ctx['_run_autotest'] = True
+        ctx['_parser'] = self.parser
+        opt_obj = self.parser.parse_args(arguments)
+        ctx['_opt_obj'] = opt_obj
+        if apply_conf:
+            if hasattr(opt_obj, 'conf_fn'):
+                ctx['conf_fn'] = opt_obj.conf_fn
+            ctx = self.read_config(ctx)
+            opt_obj = self.parser.parse_args(arguments)
+        ctx = self.create_params_dict(ctx)
+        p = 'opt_verbose'
+        if p in ctx and ctx[p] is None:
+            if os.environ.get('VERBOSE_MODE', '') in ('0', '1'):
+                ctx[p] = int(os.environ['VERBOSE_MODE'])
+            elif os.isatty(0):
+                ctx[p] = 1
+            else:
+                ctx[p] = 0
+        # elif p in ctx and ctx[p] == -1:
+        #     ctx[0] = 0
+        return ctx
