a374add196b3bf7fd9610c968d22acc27cdc5f82
zeroincombenze <info@shs-av.com>
2018-12-03 22:05:12 +0100

    [RUN] test nightly

--- install_tools.sh
+++ install_tools.sh
@@ -1,4 +1,4 @@
-# __version__=0.1.18
+# __version__=0.1.19
 THIS=$(basename "$0")
 TDIR=$(readlink -f $(dirname $0))
 if [[ $1 =~ -.*h ]]; then
@@ -9,9 +9,9 @@ if [[ $1 =~ -.*h ]]; then
   exit 0
 fi
 RFLIST__travis_emulator="cvt_csv_2_rst.py cvt_csv_2_xml.py dist_pkg gen_addons_table.py gen_readme.py odoo_translation.py please please.man please.py prjdiff replica.sh topep8 topep8.py to_pep8.2p8 to_pep8.py travis travisrc vfcp vfdiff wok_doc wok_doc.py"
-RFLIST__clodoo="awsfw clodoo.py clodoocore.py clodoolib.py inv2draft_n_restore.py list_requirements.py manage_db manage_odoo manage_odoo.man odoo_install_repository odoorc oe_watchdog run_odoo_debug odoo_skin.sh set_odoover_confn transodoo.py transodoo.csv upd_oemod.py"
+RFLIST__clodoo="awsfw . inv2draft_n_restore.py list_requirements.py manage_db manage_odoo manage_odoo.man odoo_install_repository odoorc oe_watchdog run_odoo_debug odoo_skin.sh set_odoover_confn transodoo.py transodoo.csv upd_oemod.py"
 RFLIST__zar="pg_db_active"
-RFLIST__z0lib="z0librun.py z0librc"
+RFLIST__z0lib=". z0librc"
 RFLIST__zerobug="z0testrc"
 RFLIST__wok_code="cvt_script"
 RFLIST__lisa="lisa lisa.conf.sample lisa.man lisa_bld_ods kbase/*.lish odoo-server_Debian odoo-server_RHEL"
@@ -31,11 +31,23 @@ for pkg in travis_emulator clodoo zar z0lib zerobug wok_code lisa tools; do
   flist=${!l}
   [[ ! $1 =~ -.*q ]] && echo "[$pkg=$flist]"
   for fn in $flist; do
-    [ "$pkg" != "tools" ] && src="$SRCPATH/${pkg}/$fn" || src="$SRCPATH/$fn"
-    tgt="$DSTPATH/$fn"
+    if [ "$fn" == "." ]; then
+      src="$SRCPATH/${pkg}"
+      tgt="$DSTPATH/${pkg}"
+      ftype=d
+    elif [ "$pkg" != "tools" ]; then
+      src="$SRCPATH/${pkg}/$fn"
+      tgt="$DSTPATH/$fn"
+      ftype=f
+    else
+      src="$SRCPATH/$fn"
+      tgt="$DSTPATH/$fn"
+      ftype=f
+    fi
     if $(echo "$src"|grep -Eq "\*"); then
       src=$(dirname "$src")
       tgt=$(dirname "$tgt")
+      ftype=d
     fi
     if [ ! -f "$src" -a ! -d "$src" ]; then
       echo "File $src not found!"
@@ -43,13 +55,14 @@ for pkg in travis_emulator clodoo zar z0lib zerobug wok_code lisa tools; do
       if [ -L "$tgt"  -o -f "$tgt" ]; then
         [[ ! $1 =~ -.*q ]] && echo "\$ rm -f $tgt"
         rm -f $tgt
+        [ "${tgt: -3}" == ".py" -a -f ${tgt}c ] && rm -f ${tgt}c
       fi
       [[ ! $1 =~ -.*q ]] && echo "\$ ln -s $src $tgt"
       ln -s $src $tgt
     fi
   done
 done
-for fn in addsubm.sh run_odoo_debug.sh; do
+for fn in addsubm.sh clodoo.py clodoocore.py clodoolib.py run_odoo_debug.sh z0lib.py z0lib.pyc z0librun.py z0librun.pyc; do
   tgt="$DSTPATH/$fn"
   if [ -L "$tgt"  -o -f "$tgt" ]; then
     [[ ! $1 =~ -.*q ]] && echo "\$ rm -f $tgt"
@@ -58,6 +71,8 @@ for fn in addsubm.sh run_odoo_debug.sh; do
 done
 export PYTHONPATH=$DSTPATH:$SRCPATH
 [ $(echo "$PATH"|grep -v "$DSTPATH") ] && export PATH=$DSTPATH:$PATH
+echo "--------------------------------------------------"
 echo "Please add followinf statements in your login file"
 echo "export PYTHONPATH=$DSTPATH:$SRCPATH"
 echo "export PATH=$DSTPATH:\$PATH"
+echo "--------------------------------------------------"
