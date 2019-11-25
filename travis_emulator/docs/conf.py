c16069eac7a9bf667358de1a6e4c6759752e5149
zeroincombenze <info@shs-av.com>
2019-11-25 18:48:50 +0100

    [IMP] please of travis emulator improvements

--- travis_emulator/docs/conf.py
+++ travis_emulator/docs/conf.py
@@ -25,9 +25,9 @@ copyright = '2019, Antonio Maria Vigliotti'
 author = 'Antonio Maria Vigliotti'
 
 # The short X.Y version
-version = '0.2.2.22'
+version = '0.2.2.23'
 # The full version, including alpha/beta/rc tags
-release = '0.2.2.22'
+release = '0.2.2.23'
 
 
 # -- General configuration ---------------------------------------------------
