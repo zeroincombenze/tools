530d653f9e323cc7d095380459e4bae4150f31ec
zeroincombenze <info@shs-av.com>
2020-01-06 14:04:31 +0100

    [IMP] venv_mgr upgrade

--- python_plus/__main__.py
+++ python_plus/__main__.py
@@ -10,7 +10,7 @@ import subprocess
 from zerobug import Z0BUG
 
 
-__version__ = "0.1.2.1"
+__version__ = "0.1.2.2"
 STS_FAILED = 1
 STS_SUCCESS = 0
 
--- python_plus/tests/python_plus_test_01.py
+++ python_plus/tests/python_plus_test_01.py
@@ -21,7 +21,7 @@ MODULE_ID = 'python_plus'
 TEST_FAILED = 1
 TEST_SUCCESS = 0
 
-__version__ = "0.1.2"
+__version__ = "0.1.2.2"
 
 
 def version():
