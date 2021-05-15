74c6509d170c8c8fb1e17e34bc2e0b22e883480f
zeroincombenze <info@shs-av.com>
2021-05-15 16:01:06 +0200

    [TEST] z0lib travisci test

--- z0lib/tests/test_z0lib.py
+++ z0lib/tests/test_z0lib.py
@@ -1,35 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) SHS-AV s.r.l. (<http://www.zeroincombenze.org>)
-#    All Rights Reserved
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 """
     Zeroincombenze® unit test library for python programs Regression Test Suite
 """
 # import pdb
 import os
-import sys
-from zerobug import Z0test
+# import sys
+# from zerobug import Z0test
 from z0lib import z0lib
 
 
-__version__ = "1.0.0.3"
+__version__ = "1.0.0.4"
 
 MODULE_ID = 'z0lib'
 TEST_FAILED = 1
@@ -51,9 +32,8 @@ class RegressionTest():
                                  z0lib.__version__,
                                  __version__)
         #
-        sts = TEST_SUCCESS
         parser = z0lib.parseoptargs("Unit Test",
-                                    "© 2015-2018 by SHS-AV s.r.l.",
+                                    "© 2015-2021 by SHS-AV s.r.l.",
                                     version=__version__)
         parser.add_argument('-h')
         parser.add_argument('-n')
@@ -71,9 +51,9 @@ class RegressionTest():
         return sts
 
     def test_02(self, z0ctx):
-        sts = TEST_SUCCESS
+        # sts = TEST_SUCCESS
         parser = z0lib.parseoptargs("Unit Test",
-                                    "© 2015-2018 by SHS-AV s.r.l.",
+                                    "© 2015-2021 by SHS-AV s.r.l.",
                                     version=__version__)
         parser.add_argument('-h')
         parser.add_argument('-n')
@@ -97,21 +77,21 @@ class RegressionTest():
                                  "cmd mytarget [-qv]",
                                  TRES,
                                  ctx['opt_verbose'])
-        sts = self.Z.test_result(z0ctx,
-                                 "cmd mytarget [-n]",
-                                 False,
-                                 ctx['dry_run'])
-        sts = self.Z.test_result(z0ctx,
-                                 "cmd mytarget [-p]",
-                                 '~/',
-                                 ctx['opt_path'])
-        sts = self.Z.test_result(z0ctx,
-                                 "cmd mytarget",
-                                 'mytarget',
-                                 ctx['mytarget'])
+        sts += self.Z.test_result(z0ctx,
+                                  "cmd mytarget [-n]",
+                                  False,
+                                  ctx['dry_run'])
+        sts += self.Z.test_result(z0ctx,
+                                  "cmd mytarget [-p]",
+                                  '~/',
+                                  ctx['opt_path'])
+        sts += self.Z.test_result(z0ctx,
+                                  "cmd mytarget",
+                                  'mytarget',
+                                  ctx['mytarget'])
 
         parser = z0lib.parseoptargs("Unit Test",
-                                    "© 2015-2018 by SHS-AV s.r.l.",
+                                    "© 2015-2021 by SHS-AV s.r.l.",
                                     version=__version__)
         parser.add_argument('-h')
         parser.add_argument('-n')
@@ -125,13 +105,13 @@ class RegressionTest():
         parser.add_argument('-v')
         parser.add_argument('mytarget')
         ctx = parser.parseoptargs(['-q', 'mytarget'])
-        sts = self.Z.test_result(z0ctx,
-                                 "cmd mytarget -q",
-                                 0,
-                                 ctx['opt_verbose'])
+        sts += self.Z.test_result(z0ctx,
+                                  "cmd mytarget -q",
+                                  0,
+                                  ctx['opt_verbose'])
 
         parser = z0lib.parseoptargs("Unit Test",
-                                    "© 2015-2018 by SHS-AV s.r.l.",
+                                    "© 2015-2021 by SHS-AV s.r.l.",
                                     version=__version__)
         parser.add_argument('-h')
         parser.add_argument('-n')
@@ -145,17 +125,17 @@ class RegressionTest():
         parser.add_argument('-v')
         parser.add_argument('mytarget')
         ctx = parser.parseoptargs(['-v', 'mytarget'])
-        sts = self.Z.test_result(z0ctx,
-                                 "cmd mytarget -v",
-                                 1,
-                                 ctx['opt_verbose'])
+        sts += self.Z.test_result(z0ctx,
+                                  "cmd mytarget -v",
+                                  1,
+                                  ctx['opt_verbose'])
         return sts
 
 
 #
 # Run main if executed as a script
-if __name__ == "__main__":
-    Z = Z0test
-    ctx = Z.parseoptest(sys.argv[1:],
-                        version=version())
-    exit(Z.main(ctx, RegressionTest))
+# if __name__ == "__main__":
+#     Z = Z0test
+#     ctx = Z.parseoptest(sys.argv[1:],
+#                         version=version())
+#     exit(Z.main(ctx, RegressionTest))
