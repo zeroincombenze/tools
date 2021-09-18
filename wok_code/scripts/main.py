056a1106e835ac1fb6e62133aad1cb9d1b04d75b
zeroincombenze <info@shs-av.com>
2021-09-18 22:06:07 +0200

    [IMP] wok_code stable version

---
+++ wok_code/scripts/main.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Various tools at your fingertips.
+
+The available tools are:
+
+* cvt_csv_2_rst.py: convert csv file into rst file
+* cvt_csv_2_xml.py: convert csv file into xml file
+* cvt_script: parse bash script and convert to meet company standard
+* gen_readme.py: generate documentation files, mainly README.rst
+* odoo_dependency.py: show odoo depencies and/or Odoo module tree
+* odoo_translation.py: manage Odoo translation
+* pep8: parse source .py file to meet pep8 and convert across Odoo versions
+* please: developer shell
+* wget_odoo_repositories.py: get repository names from github.com
+"""
+import os
+import sys
+
+
+__version__ = '1.0.2.99'
+
+
+def fake_setup(**kwargs):
+    globals()['setup_args'] = kwargs
+
+
+def read_setup():
+    to_copy = False
+    setup_file = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), '..', 'setup.py'))
+    setup_bup = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), 'setup.py'))
+    if not os.path.isfile(setup_file):
+        setup_file = setup_bup
+    elif os.path.isfile((setup_bup)):
+        to_copy = True
+    if os.path.isfile(setup_file):
+        with open(setup_file, 'r') as fd:
+            content = fd.read()
+            if to_copy:
+                with open(setup_bup) as fd2:
+                    fd2.write(content)
+            content = content.replace('setup(', 'fake_setup(')
+            exec(content)
+    return globals()['setup_args']
+
+
+def main(cli_args=None):
+    if not cli_args:
+        cli_args = sys.argv[1:]
+    action = False if not cli_args else cli_args[0]
+    setup_args = read_setup()
+    if action == '-h':
+        print('%s [-h] [--help] [-H] [-V]' % setup_args['name'])
+        return 0
+    if action not in ('-H', '--help'):
+        if setup_args['version'] == __version__:
+            print(setup_args['version'])
+        else:
+            print('Version mismatch %s/%s' % (setup_args['version'],
+                                              __version__))
+    if action != '-V':
+        for text in __doc__.split('\n'):
+            print(text)
+    return 0
--- wok_code/setup.py
+++ wok_code/setup.py
@@ -1,24 +1,53 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup
+from setuptools import find_packages
 
-setup(name='devel_tools',
+setup(name='wok_code',
       version='1.0.2.2',
       description='Python developers tools',
       long_description="""
-Tool to parse, edit and migrate python source code
+Various tools at your fingertips.
+
+The available tools are:
+
+* cvt_csv_2_rst.py: convert csv file into rst file
+* cvt_csv_2_xml.py: convert csv file into xml file
+* cvt_script: parse bash script and convert to meet company standard
+* gen_readme.py: generate documentation files, mainly README.rst
+* odoo_dependency.py: show odoo depencies and/or Odoo module tree
+* odoo_translation.py: manage Odoo translation
+* pep8: parse source .py file to meet pep8 and convert across Odoo versions
+* please: developer shell
+* wget_odoo_repositories.py: get repository names from github.com
 """,
       classifiers=[
           'Development Status :: 3 - Alpha',
           'License :: OSI Approved :: GNU Affero General Public License v3',
+          'Operating System :: POSIX',
           'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
           'Intended Audience :: Developers',
           'Topic :: Software Development',
           'Topic :: Software Development :: Build Tools',
           'Operating System :: OS Independent',
       ],
       keywords='linux travis development',
-      url='http://wiki.zeroincombenze.org/',
+      project_urls={
+          'Documentation': 'https://zeroincombenze-tools.readthedocs.io',
+          'Source': 'https://github.com/zeroincombenze/tools',
+      },
       author='Antonio Maria Vigliotti',
       author_email='antoniomaria.vigliotti@gmail.com',
       license='Affero GPL',
-      packages=['devel_tools'],
+      packages=find_packages(
+          exclude=['docs', 'examples', 'tests', 'egg-info', 'junk']),
+      package_data={
+          '': ['./cvt_script']
+      },
+      entry_points={
+          'console_scripts': [
+              'wok_code = wok_code.scripts.main:main'
+          ],
+      },
       zip_safe=False)
