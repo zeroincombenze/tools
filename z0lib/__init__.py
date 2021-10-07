0e71c4b39df70ca990b86298bd064be438bb4ac6
zeroincombenze <info@shs-av.com>
2021-10-07 21:22:04 +0200

    [IMP] refactor z0lib

--- z0lib/__init__.py
+++ z0lib/__init__.py
@@ -1,4 +1,1 @@
-try:
-    from . import z0librun as z0lib
-except ImportError:
-    from z0lib import z0librun as z0lib
+from . import scripts
--- z0lib/tests/test_z0lib.py
+++ z0lib/tests/test_z0lib.py
@@ -1,4 +1,4 @@
-#!/home/odoo/devel/venv/bin/python2
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
     Zeroincombenze® unit test library for python programs Regression Test Suite
