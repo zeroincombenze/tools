5daec059bf38b17141d180b9e51e58fdeef0bf35
zeroincombenze <info@shs-av.com>
2021-11-11 21:11:46 +0100

    [IMP] clodoo improvements

--- clodoo/tests/__clodoo_test_04.py
+++ clodoo/tests/__clodoo_test_04.py
@@ -28,7 +28,7 @@ except BaseException:
     from clodoolib import crypt
 
 
-__version__ = "0.3.36.2"
+__version__ = "0.3.53.1"
 
 
 MODULE_ID = 'clodoo'
--- clodoo/tests/clodoo_test_01.py
+++ clodoo/tests/clodoo_test_01.py
@@ -34,7 +34,7 @@ except BaseException:
 from zerobug import z0test
 
 
-__version__ = "0.3.36.2"
+__version__ = "0.3.53.1"
 
 MODULE_ID = 'clodoo'
 TEST_FAILED = 1
