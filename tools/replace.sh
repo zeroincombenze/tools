de5f44fd1c5fa8babc5d62ee9f2cda82a45ee457
zeroincombenze <info@shs-av.com>
2023-12-03 15:36:36 +0100

    [IMP] Readthedocs documentation

--- tools/replace.sh
+++ tools/replace.sh
@@ -3,7 +3,7 @@ if [[ -z $HOME_DEVEL ]]; then
     [[ -d $HOME/odoo/devel ]] && HOME_DEVEL="$HOME/odoo/devel" || HOME_DEVEL="$HOME/devel"
 fi
 tgtdir=$(readlink -f $HOME_DEVEL/..)/tools
-for f in .travis.yml install_tools.sh LICENSE odoo_template_tnl.xlsx README.rst; do
+for f in install_tools.sh LICENSE odoo_template_tnl.xlsx README.rst; do
     echo "\$ cp ./$f $tgtdir/"
     cp ./$f $tgtdir/
 done
@@ -11,6 +11,8 @@ for item in egg-info docs tests templates license_text; do
     echo "\$ rsync -a ./$item/ $tgtdir/$item/"
     rsync -a ./$item/ $tgtdir/$item/
 done
+echo "\$ cp ../.readthedocs.yml $tgtdir/"
+cp ../.readthedocs.yml $tgtdir/
 [[ -f $tgtdir/install_foreign.sh ]] && rm -f $tgtdir/install_foreign.sh
 # cd $tgtdir
 # ./install_tools.sh -T
