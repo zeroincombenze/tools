75404f1caf5b58edb0fdbd5b1704d471127756c0
zeroincombenze <info@shs-av.com>
2024-08-22 08:36:18 +0200

    [IMP] package versions improvements

--- python_plus/setup.py
+++ python_plus/setup.py
@@ -44,7 +44,7 @@ setup(
     author=author,
     author_email=author_email,
     license="Affero GPL",
-    install_requires=["configparser", "future", "z0lib"],
+    install_requires=["configparser", "future", "z0lib>=2.0.11"],
     packages=find_packages(exclude=["docs", "examples", "tests", "egg-info", "junk"]),
     package_data={"": ["scripts/setup.info", "scripts/vem.sh", "./vem.man"]},
     entry_points={
